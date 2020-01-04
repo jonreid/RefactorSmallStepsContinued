c1d449088521cfbddb9f5183fe5c2ff9151cd1f6
Jon Reid <jon@qualitycoding.org>
2020-01-04 10:57:33 -0800

    Rename project

--- Tests/ViewControllerTests.swift
+++ Tests/ViewControllerTests.swift
@@ -1,7 +1,7 @@
 //  Refactoring Clean Mess by Jon Reid, https://qualitycoding.org/refactoring-clean-mess/
 //  Copyright 2018 Quality Coding, Inc. See LICENSE.txt
 
-@testable import RefactorSmallStepsContinued
+@testable import RefactoringCleanMess
 import XCTest
 
 final class ViewControllerTests: XCTestCase {
